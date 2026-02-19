ce39c3e31a60568b502b0ec37ed101eb5e6b7c0c
Nathi2266 <Nathiradebe71@gmail.com>
2026-02-19 07:27:59 +0200

    Version control update

--- lib/widgets/version_control_widget.dart
+++ lib/widgets/version_control_widget.dart
@@ -35,21 +35,17 @@ class _VersionControlWidgetState extends State<VersionControlWidget>
       vsync: this,
     );
 
-    _scaleAnimation = Tween<double>(
-      begin: 1.0,
-      end: 1.05,
-    ).animate(CurvedAnimation(
-      parent: _animationController,
-      curve: Curves.easeInOut,
-    ));
+    _scaleAnimation = Tween<double>(begin: 1.0, end: 1.05).animate(
+      CurvedAnimation(parent: _animationController, curve: Curves.easeInOut),
+    );
 
-    _colorAnimation = ColorTween(
-      begin: widget.textColor,
-      end: widget.hoverColor,
-    ).animate(CurvedAnimation(
-      parent: _animationController,
-      curve: Curves.easeInOut,
-    ));
+    _colorAnimation =
+        ColorTween(begin: widget.textColor, end: widget.hoverColor).animate(
+          CurvedAnimation(
+            parent: _animationController,
+            curve: Curves.easeInOut,
+          ),
+        );
   }
 
   @override
@@ -71,23 +67,46 @@ class _VersionControlWidgetState extends State<VersionControlWidget>
     return MouseRegion(
       onEnter: (_) => _onHover(true),
       onExit: (_) => _onHover(false),
-      child: AnimatedBuilder(
-        animation: _animationController,
-        builder: (context, child) {
-          return Transform.scale(
-            scale: _scaleAnimation.value,
-            child: Text(
-              widget.version,
-              style: TextStyle(
-                fontSize: widget.fontSize,
-                color: _colorAnimation.value,
-                fontWeight: FontWeight.w400,
-                fontFamily: 'Inter',
-                letterSpacing: 0.5,
+      child: Tooltip(
+        message:
+            'Daily Commits - ${widget.version}\n\n'
+            '1. Nathi - Fix Export Button Functionality on Settings Screen\n\n'
+            '2. Lihle - Sidebar Navigation Order for Improved Information Hierarchy.\n\n'
+            '3. Sipho - Refactor UI Manager Sidebar Icons',
+        textAlign: TextAlign.left,
+        padding: const EdgeInsets.all(16),
+        margin: const EdgeInsets.symmetric(horizontal: 20),
+        decoration: BoxDecoration(
+          color: Colors.black.withOpacity(0.9),
+          borderRadius: BorderRadius.circular(8),
+          border: Border.all(color: Colors.white.withOpacity(0.2)),
+        ),
+        textStyle: const TextStyle(
+          color: Colors.white,
+          fontSize: 12,
+          fontFamily: 'Inter',
+          height: 1.4,
+        ),
+        showDuration: const Duration(seconds: 10),
+        waitDuration: const Duration(milliseconds: 500),
+        child: AnimatedBuilder(
+          animation: _animationController,
+          builder: (context, child) {
+            return Transform.scale(
+              scale: _scaleAnimation.value,
+              child: Text(
+                widget.version,
+                style: TextStyle(
+                  fontSize: widget.fontSize,
+                  color: _colorAnimation.value,
+                  fontWeight: FontWeight.w400,
+                  fontFamily: 'Inter',
+                  letterSpacing: 0.5,
+                ),
               ),
-            ),
-          );
-        },
+            );
+          },
+        ),
       ),
     );
   }
